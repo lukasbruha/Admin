8b25ee98c28128959a7ecc3caa0a61d309064f74
Daaarkling <jano.vano@gmail.com>
2016-02-20 19:53:15 +0100

    js tabs link

--- src/Kollarovic/Admin/assets/admin.js
+++ src/Kollarovic/Admin/assets/admin.js
@@ -8,11 +8,31 @@ $(document).ready(function () {
 		stepMinute: 5
 	});
 	
+	// Javascript to enable link to tab
+	var hash = document.location.hash;
+	var prefix = "tab_";
+	if (hash) {
+		console.log('.nav-tabs a[href=#tab_1-3]');
+		console.log($('.nav-tabs a[href=#tab_1-3]').size());
+		
+		console.log('.nav-tabs a[href='+hash.replace(prefix,"")+']');
+		console.log($('.nav-tabs a[href='+hash.replace(prefix,"")+']').size());
+		
+		$('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+	} 
+
+	//-------------------------
+	// Change hash for page-reload
+	$('.nav-tabs a').on('shown', function (e) {
+		window.location.hash = e.target.hash.replace("#", "#" + prefix);
+	});
+	
 	
 	$('a[data-toggle="tab"]').click(function() {
 		var title = $(this).text();
 		$('#tab-caption').text(title);
 	});
+	//-------------------------
 
 
 	jQuery('.login-form').show();
@@ -27,5 +47,8 @@ $(document).ready(function () {
 		jQuery('.login-form').show();
 		jQuery('.forget-form').hide();
 	});
+
+
+	
 
 });	
